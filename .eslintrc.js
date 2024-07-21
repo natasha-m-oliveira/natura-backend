28a51b10d1d4c5a4fd2f30d30dc85dc805904c75
natasha-m-oliveira <me@natashamo.dev>
2024-07-21 11:09:06 -0300

    chore: adjust Jest and ESLint configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-import-helpers'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings'
   ],
   root: true,
   env: {
@@ -17,9 +19,46 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: true,
+        endOfLine: 'lf',
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+          'newlinesBetween': 'always',
+          'groups': [
+              'module',
+              '/@nestjs/',
+              '/^@/',
+              [
+                  'parent',
+                  'sibling',
+                  'index'
+              ]
+          ],
+          'alphabetize': {
+              'order': 'asc',
+              'ignoreCase': true
+          }
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      'typescript': {},
+    },
   },
 };
